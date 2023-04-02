a1aed3020a9034d66fa220a7bf01eb9398fb2ca4
jaye-jaye <823701573@qq.com>
2023-04-02 19:17:51 +0800

    update hdfs

--- hdfs/docker/journalnode/run.sh
+++ hdfs/docker/journalnode/run.sh
@@ -5,12 +5,12 @@ function path_fix() {
 }
 
 VERSION_DIR=$(path_fix ${HDFS_CONF_dfs_journalnode_edits_dir}/${HDFS_CONF_dfs_nameservices}/current)
-if [ ! -d $VERSION_DIR ]; then
-    echo "create version dir $VERSION_DIR"
-    mkdir -p $VERSION_DIR
-    if [ -f /etc/hadoop/VERSION.backup ]; then
-        cp /etc/hadoop/VERSION.backup $VERSION_DIR/VERSION
+if [ -f /etc/hadoop/VERSION.backup ]; then
+    if [ ! -d $VERSION_DIR ]; then
+        echo "create version dir $VERSION_DIR"
+        mkdir -p $VERSION_DIR
     fi
+    cp /etc/hadoop/VERSION.backup $VERSION_DIR/VERSION
 fi
 
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR journalnode
